6adc2cab8ada1584d99737e28779b210ac469547
Ariana Rahman <arianarahman035@gmail.com>
2026-06-06 21:47:35 +0530

    reduce memory usage for free tier

--- services/embedder.py
+++ services/embedder.py
@@ -1,16 +1,25 @@
-from sentence_transformers import SentenceTransformer
 import numpy as np
+from sentence_transformers import SentenceTransformer
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 #MODEL_NAME = "all-MiniLM-L6-v2"
-MODEL_NAME = "paraphrase-MiniLM-L3-v2"
+#MODEL_NAME = "paraphrase-MiniLM-L3-v2"
+MODEL_NAME = "sentence-transformers/paraphrase-albert-small-v2"
 
-# Load immediately when the module is imported
+
 print(f"Loading model: {MODEL_NAME} ...")
-_model = SentenceTransformer(MODEL_NAME)
-print("Model loaded successfully.")
+_model = SentenceTransformer(MODEL_NAME, device="cpu")
+print("Model loaded.")
 
 def get_model():
     return _model
 
 def encode(texts: list[str]) -> np.ndarray:
-    return _model.encode(texts, convert_to_numpy=True, normalize_embeddings=True)
+    return _model.encode(
+        texts,
+        convert_to_numpy=True,
+        normalize_embeddings=True,
+        batch_size=4
+    )
